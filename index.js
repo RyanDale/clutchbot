9f955d60e968cf3b39c2082b5c243949b79d8312
Your Correct Name <contact@ryan-dale.com>
2019-06-17 23:18:45 -0400

    Initial commit of Clutchbot.

---
+++ index.js
@@ -0,0 +1,72 @@
+require('dotenv').config();
+
+const Botkit = require('botkit');
+
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
+    console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+    process.exit(1);
+}
+
+let config = {};
+if (process.env.MONGOLAB_URI) {
+    const BotkitStorage = require('botkit-storage-mongo');
+    config = {
+        storage: BotkitStorage({ mongoUri: process.env.MONGOLAB_URI }),
+    };
+} else {
+    config = {
+        json_file_store: './db_slackbutton_slash_command/',
+    };
+}
+
+const controller = Botkit.slackbot(config).configureSlackApp(
+    {
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        scopes: ['commands'],
+    }
+);
+
+controller.setupWebserver(process.env.PORT, function (err, webserver) {
+    controller.createWebhookEndpoints(controller.webserver);
+
+    controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
+        if (err) {
+            res.status(500).send('ERROR: ' + err);
+        } else {
+            res.send('Success!');
+        }
+    });
+});
+
+controller.on('slash_command', (slashCommand, message) => {
+    // Invalid token
+    if (message.token !== process.env.VERIFICATION_TOKEN) {
+        return;
+    }
+
+    switch (message.command) {
+        case "/find":
+            if (message.text === "" || message.text === "help") {
+                slashCommand.replyPrivate(message, "Find a player or strategy card.");
+                return;
+            }
+
+            const name = message.text;
+            const fileName = name.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+            const card = {
+                attachments: [
+                    {
+                        fallback: "Player card not found",
+                        pretext: name,
+                        image_url: "https://clutchmoment.com/wp-content/uploads/2019/06/All-Star_Pitchers8-copy.png"
+                    }
+                ]
+            };
+            slashCommand.replyPublic(message, card);
+            break;
+        default:
+            slashCommand.replyPublic(message, "Command not recognized.");
+    }
+
+});
