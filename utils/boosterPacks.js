f8ff2bd856c3a2f5f543e8841cb6f73a8e66aa6b
Ryan Dale <contact@ryan-dale.com>
2019-06-25 20:48:45 -0400

    Refactor logic. Add step 10.

--- utils/boosterPacks.js
+++ utils/boosterPacks.js
@@ -1,16 +1,17 @@
 const _ = require('lodash');
 const Card = require('../models/Card');
 
-const getCard = async args => await Card.aggregate([
-    { $match: args },
-    { $sample: { size: 1 } }
-]);
+const getCard = async args => {
+    const cards = await Card.aggregate([
+        { $match: args },
+        { $sample: { size: 1 } }
+    ]);
+    return cards[0];
+}
 
 /*
 Clutch Digital 2019 Series 1 Pack Building Guide:
 
-Card 10 – One random uncommon player from 102 or 103 cards – 50 or 51 pitchers, 52 batters (removing cards 13, 12 and 11 from the possibilities)
-    (if a pitcher was chosen for card 11 then its automatically a batter)
 Card 9 – One random common strat from 40 cards
 Card 8 – One random common pitcher from 41 cards
 Card 7 – One random common pitcher from 41 cards
@@ -25,25 +26,35 @@ Card 1 – One random common from 238 or 239 cards – 119 or 120 common pitcher
 */
 const series1PackBuilder = {
     // Card 15 – One random rare from 38 cards – 25 rare pitchers, 10 rare strats, 3 rare stadiums
-    async card15(kwargs = {}) {
+    async card15() {
         return await getCard({
             year: '2019',
             series: 'Series 1',
             rarity: 'R',
-            ...kwargs
+            cardType: {
+                '$in': ['Pitcher', 'Strategy', 'Stadium'],
+            }
         });
     },
 
     // Card 14 – One random rare from 50 cards – 25 rare pitchers, 25 rare batters
     //    (if a pitcher was chosen for card 15 then its automatically a batter)
     async card14(pack) {
-        let kwargs = {
-            _id: { $ne: pack[0]._id }
-        };
-        if (pack[0].cardType === 'Pitcher') {
-            kwargs.cardType = 'Batter';
+        let cardTypes = ['Batter'];
+
+        // Allow a pitcher to be chosen if one wasn't chosen for Card 15
+        if (pack[0].cardType !== 'Pitcher') {
+            cardTypes.push('Pitcher');
         }
-        return await await this.card15(kwargs);
+        return await getCard({
+            year: '2019',
+            series: 'Series 1',
+            rarity: 'R',
+            _id: { $ne: pack[0]._id },
+            cardType: {
+                '$in': cardTypes
+            }
+        });
     },
 
     // Card 13 – One random uncommon pitcher from 52 cards (uncommon pitchers)
@@ -78,6 +89,22 @@ const series1PackBuilder = {
             },
             // pack[2] = Card 13
             _id: { $ne: pack[2]._id }
+        });
+    },
+
+    // Card 10 – One random uncommon player from 102 or 103 cards – 50 or 51 pitchers, 52 batters
+    //  (removing cards 13, 12 and 11 from the possibilities)
+    //  (if a pitcher was chosen for card 11 then its automatically a batter)
+    async card10(pack) {
+        // pack[4] = Card 11
+        const cardType = pack[4].cardType === 'Pitcher' ? 'Batter' : { '$in': ['Pitcher', 'Batter'] };
+
+        return await getCard({
+            year: '2019',
+            series: 'Series 1',
+            rarity: 'U',
+            cardType,
+            _id: { $nin: pack.map(card => card._id) }
         });
     },
 };
